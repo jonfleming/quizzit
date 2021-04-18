0bab5997ae4426dc63f6d657c2124d7f41938cab
Jon Fleming <jonfleming@hotmail.com>
2021-04-18 00:16:32 -0700

    52 Questions

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <h1 class="left">Driver Knowledge Practice Test</h1>
+  <h3 class="left">(52 Questions)</h3>
   <button v-if="questionsLoaded && !started && !done" v-on:click="start()">Start</button>
   <question v-if="started && !done" v-bind:question="questionList[questionNumber]" @correct="increaseScore()" @incorrect="nextQuestion()"/>
   <p v-if="done" class="left">Your got {{correct}} out of {{questionList.length - 1}}</p>
